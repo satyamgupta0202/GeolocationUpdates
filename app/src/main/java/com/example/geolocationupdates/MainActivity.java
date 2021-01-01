a7a44a69a15a8c296b9c8e9bfa5825b624506712
satyam <2019335@iiitdmj.ac.in.com>
2021-01-01 18:19:36 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/geolocationupdates/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.geolocationupdates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        ArrayList<String> city = new ArrayList<String>();
+        city.add("Add item to the android list");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 ,city);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, city.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
